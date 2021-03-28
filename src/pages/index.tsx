e82e8b96ec0631f85c01fef71f17e443e15c0bf0
tuboihirokidesu <tuboi.hiroki@icloud.com>
2021-03-28 20:56:14 +0900

    Layoutの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,56 @@
 import React from 'react'
-import Head from 'next/head'
 import HeroSection from '../components/HeroSection'
 import { HeroParagraph, HeroHeader } from '../styles/pages/HeroSection'
 import Navbar from '../components/Navbar'
+import styled from 'styled-components'
+import Particles from 'react-particles-js'
+import Layout from '../components/Layout'
+
+const Particle = styled(Particles)`
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  z-index: 1;
+`
 
 const Home: React.FC = () => {
   return (
     <>
-      <Head>
-        <title>Homepage</title>
-      </Head>
-      <main>
-        <Navbar />
-        <HeroSection>
-          <HeroHeader strings={['Portfolio']} typeSpeed={80} />
-          <HeroParagraph
-            strings={['Hiroki', 'Tsuboi']}
-            typeSpeed={70}
-            backSpeed={40}
-            loop
+      <Layout title="Home Page">
+        <main>
+          <Particle
+            params={{
+              particles: {
+                number: {
+                  value: 30,
+                  density: {
+                    enable: true,
+                    value_area: 900
+                  }
+                },
+                shape: {
+                  type: 'circle',
+                  stroke: {
+                    width: 6,
+                    color: '#f9ab00'
+                  }
+                }
+              }
+            }}
           />
-        </HeroSection>
-      </main>
+
+          <Navbar />
+          <HeroSection>
+            <HeroHeader strings={['Portfolio']} typeSpeed={80} />
+            <HeroParagraph
+              strings={['Hiroki', 'Tsuboi']}
+              typeSpeed={70}
+              backSpeed={40}
+              loop
+            />
+          </HeroSection>
+        </main>
+      </Layout>
     </>
   )
 }
